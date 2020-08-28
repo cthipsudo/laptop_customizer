b79df4dda0185a2e1602ebc327daa6bc76aafa63
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2020-08-28 17:29:28 -0500

    Initial dist subtree commit

---
+++ build/precache-manifest.1d824181286ae7c4d35b13fdaa0c9f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4cfa750bf7625930aa4d97fef4f344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c354746816f5f3a5d6d",
+    "url": "/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "2c354746816f5f3a5d6d",
+    "url": "/static/js/main.9a9a59e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
